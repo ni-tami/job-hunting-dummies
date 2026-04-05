22fb58687913a9b906936e848fec3e173859ef86
ni-tami <n.c.utami@gmail.com>
2026-04-05 15:16:23 +0700

    change pyworker workflow cycle

--- workflows/app/populate_company/worker/populate_company_worker.py
+++ workflows/app/populate_company/worker/populate_company_worker.py
@@ -35,16 +35,21 @@ async def main():
     config.setdefault("target_host", "localhost:7233")
     client = await Client.connect(**config)
 
+    workflow_id = f"{workflow_name}-{
+        ''.join(random.choices(string.ascii_uppercase + string.digits, k=30))
+    }"
     # Run activity worker
+    # NOTE: this exexution create workflow and terminate on every run, 
+    # leaving terminated workflow runs on each stop.
     async with Worker(
         client, task_queue=task_queue, activities=[generate_company_activity]
     ):
-        workflow_id = f"{workflow_name}-{
-            ''.join(random.choices(string.ascii_uppercase + string.digits, k=30))
-        }"
-        await client.execute_workflow(
-            workflow_name, id=workflow_id, task_queue=task_queue
-        )
+        try:
+            await client.execute_workflow(
+                workflow_name, id=workflow_id, task_queue=task_queue
+            )
+        finally:
+            await client.get_workflow_handle(workflow_id=workflow_id).terminate()
 
 
 if __name__ == "__main__":
